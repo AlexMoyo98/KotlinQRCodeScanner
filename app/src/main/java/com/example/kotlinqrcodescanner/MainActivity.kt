2230926f733e54c387ce1cdcbb10b9f4f1c51556
Alex Moyo <76572132+AlexMoyo98@users.noreply.github.com>
2024-09-03 14:34:24 +0200

    Add QR code scanning functionality

---
+++ app/src/main/java/com/example/kotlinqrcodescanner/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.kotlinqrcodescanner
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.kotlinqrcodescanner.databinding.ActivityMainBinding
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
+class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                showCamera()
+            } else {
+
+            }
+        }
+
+    private val scanLauncher =
+        registerForActivityResult(ScanContract()) { result: ScanIntentResult ->
+            run {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    setResult(result.contents)
+                }
+            }
+        }
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var textResult: TextView
+
+    private fun setResult(string: String?) {
+        if (binding.scanResult != null) {
+            textResult.text = string
+        }
+    }
+
+    private fun showCamera() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan QR code")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+        options.setOrientationLocked(false)
+        scanLauncher.launch(options)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initBinding()
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.fab.setOnClickListener {
+            checkPermissionCamera(this)
+        }
+    }
+
+    private fun checkPermissionCamera(context: Context) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            showCamera()
+        } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+            Toast.makeText(context, "CAMERA permission required", Toast.LENGTH_SHORT).show()
+        } else {
+            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+        }
+    }
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        textResult = binding.scanResult
+    }
+}
